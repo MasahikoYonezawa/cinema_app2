ce925b34f266562112c3c3cd988c07bf8f8312f6
EC2 Default User <ec2-user@ip-172-31-39-112.us-east-2.compute.internal>
2018-02-03 06:02:15 +0000

    second commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sheltered-scrubland-26693.herokuapp.com'
+  host = 'secret-chamber-66484.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
